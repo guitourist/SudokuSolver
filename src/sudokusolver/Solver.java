8691145bc719f64f46d1b8b1cecdf1ad78b2c514
marshallringwood <marshallringwood@gmail.com>
2021-04-21 21:37:47 -0600

    Finished translating Solver class

--- src/sudokusolver/Solver.java
+++ src/sudokusolver/Solver.java
@@ -1,44 +1,68 @@
 package sudokusolver;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import edu.princeton.cs.algs4.ST;
-
 public class Solver
 {
-	private Board board;
+	private int[][] grid;
 	private int[] col = new int[9];
 	private int[] row = new int[9];
 	private int[][] box = new int[3][3];
 	
 	public Solver(Board board)
 	{
-		this.board = board;
+		grid = board.grid;
 		// need to make this immutable and return a copy in board class
-		int[][] grid = board.grid;
-		
-		for (int r = 0; r < 9; ++r)
+	}
+	
+	public void solveSudoku()
+	{
+		for (int i = 0; i < 9; ++i)
 		{
-            for (int c = 0; c < 9; ++c)
+            for (int j = 0; j < 9; ++j)
             {
-                if (grid[r][c] != '0')
+                if (grid[i][j] != '0')
                 {
-                    int x = grid[r][c];
-                    col[c] = x;
-                    row[r] = x;
-                    box[r/3][c/3] = x;
+                    int x = grid[i][j];
+                    col[j] = x;
+                    row[i] = x;
+                    box[i/3][j/3] = x;
                 }
             }
 		}
 		
-		
-		
+		dfs(grid, 0, 0);
+	}
+
+	private boolean dfs(int[][] grid, int i, int j)
+	{
+		if (j == 9) {
+            i++;
+            j = 0;
+        }
+        if (i == 9) {
+            return true;
+        }       
+        if (grid[i][j] != '.') {
+            return dfs(grid, i, j + 1);
+        }
+        for (int k = 1; k <= 9; ++k) {
+            if (check(k, i, j)) {
+                grid[i][j] = (char)(48 + k);
+                col[j] = k;
+                row[i] = k;
+                box[i/3][j/3] = k;
+                if (dfs(grid, i, j + 1)) {                  
+                    return true;
+                }
+                grid[i][j] = 0;
+                col[j] = 0;
+                row[i] = 0;       
+                box[i/3][j/3] = 0;
+            }
+        }
+        return false;
 	}
 	
-	
-	
-	
-    
-	
+	private boolean check(int x, int i, int j) {
+        return (col[j] != x) && (row[i] != x) && (box[i/3][j/3] != x);
+    }    
 }
